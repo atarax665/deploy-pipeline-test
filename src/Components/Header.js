d9b5086a3abf83fe9a175e0de6c741e9616dd252
Abhinav <abhinav.aa.anand@mercedes-benz.com>
2024-11-04 21:39:50 +0530

    Changed heading

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,18 @@
 const Header = () => {
-    return (
-        <div className="flex justify-between py-4 bg-pink-200 shadow-lg">
-                <img src="https://www.slido.com/static/slido-logo.a3d453e1.svg" alt="slido logo" className="w-56"/>
-                <div className="flex items-center">
-                    <ul className="flex  p-4">
-                        <li className="p-4">Features</li>
-                        <li className="p-4">Pricing</li>
-                        <li className="p-4">Resources</li>
-                    </ul>
-                </div>
-            </div>
-    );
-}
+  return (
+    <div className="flex justify-between py-4 bg-pink-200 shadow-lg">
+      <img
+        src="https://www.slido.com/static/slido-logo.a3d453e1.svg"
+        alt="slido logo"
+        className="w-56"
+      />
+      <div className="flex items-center">
+        <ul className="flex  p-4">
+          <li className="p-4">More info</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
